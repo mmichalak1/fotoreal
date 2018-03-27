3c7d62b7fa4c532fe136bf5f63f6f3e71cecf364
Xsior <195583@edu.p.lodz.pl>
2018-03-27 22:04:32 +0200

    ???

--- renderer.py
+++ renderer.py
@@ -23,7 +23,7 @@ objects = []
 objects.append(sphere(vector(0,0,600), 50, Color("Red")))
 objects.append(sphere(vector(20, 20, 580), 30, Color("Green")))
 # objects.append(sphere(vector())
-objects.append(plane(vector(0,0,800), vector(0,0,1).normalize(), Color("Blue")))
+objects.append(plane(vector(0,-29,800), vector(0,1,0).normalize(), Color("Blue")))
 
 def numColToFloat(color):
 	return tuple(x/256. for x in color.rgb)
@@ -63,7 +63,7 @@ def render(objects, camera):
 	img.show()
 	# img.save("Hello.bmp")
 
-cam = ortocam(vector(), vector(0,0,1), vector(0, 1, 0), 10000, ORTOSIZEX, ORTOSIZEY, XSTEP, YSTEP, al)
-# cam = perspectiveCam(vector(), vector(0,0,1), vector(0,1,0),10000, 500, 90, ORTOSIZEX, ORTOSIZEY, XSTEP, YSTEP, al)
+#cam = ortocam(vector(), vector(0,0,1), vector(0, 1, 0), 10000, ORTOSIZEX, ORTOSIZEY, XSTEP, YSTEP, al)
+cam = perspectiveCam(vector(), vector(0,0,1), vector(0,1,0),10000, 10, 90, ORTOSIZEX, ORTOSIZEY, XSTEP, YSTEP, al)
 
 render(objects, cam)
